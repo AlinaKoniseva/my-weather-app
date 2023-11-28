f1fa205f4b2db42dd30ac84d1da44e016e1bcfdf
AlinaKnsv <alina.knsv@gmail.com>
2023-11-28 14:46:55 +0100

    added next task description

--- script.js
+++ script.js
@@ -87,3 +87,7 @@ hour.innerHTML = now.getHours();
 
 let minute = document.querySelector(".minutes");
 minute.innerHTML = now.getMinutes();
+
+//Improve the project including the search engine, API integration, wind speed, weather description, and weather icon are mandatory. The project should not include the forecast yet.
+
+//Host your project on Netlfiy, and at the bottom of your page, link to your GitHub repository.
