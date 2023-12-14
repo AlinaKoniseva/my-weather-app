20b76792548a6aaf213d8de130b252a2ddeb93c2
AlinaKoniseva <151017402+AlinaKoniseva@users.noreply.github.com>
2023-12-14 10:45:41 +0100

    Inject the forecast HTML from JavaScript

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function(day){
+    forecastHtml = forecastHtml +
+    `
+    <div class="weather-forecast-day" >
+      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-icon">🌤️</div>
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">12°C</span>
+        <span class="weather-forecast-temperature-min">8°C</span>
+      </div>
+      </div>
+    `;
+  })
+  
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
+displayForecast();
