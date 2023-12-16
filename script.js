92f3fbf70c95ffce680d20ad7723c66acdcbbe99
AlinaKoniseva <151017402+AlinaKoniseva@users.noreply.github.com>
2023-12-16 15:04:34 +0100

    keep on optimizing HTML, CSS, JS

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-
 function displayForecast() {
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function(day){
-    forecastHtml = forecastHtml +
+  days.forEach(function(day) {
+    forecastHtml = forecastHtml += 
     `
-    <div class="weather-forecast-day" >
-      <div class="weather-forecast-date">${day}</div>
-      <div class="weather-forecast-icon">🌤️</div>
-      <div class="weather-forecast-temperatures">
-        <span class="weather-forecast-temperature-max">12°C</span>
-        <span class="weather-forecast-temperature-min">8°C</span>
+      <div class="container weather-forecast">
+        <div class="row">
+          <div class="col">${day}</div>
+          <div class="weather-forecast-icon">🌤️</div>
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">12°C</span>
+            <span class="weather-forecast-temperature-min">8°C</span>
+          </div>
+        </div>
       </div>
-      </div>
-    `;
-  })
+      `;
+  });
 
-let forecastElement = document.querySelector("#forecast");
-forecastElement.innerHTML = forecastHtml;
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
 displayForecast();
